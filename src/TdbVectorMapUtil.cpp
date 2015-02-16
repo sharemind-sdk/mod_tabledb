dee8fa73423847f53ed18034ba32e9ea05dd8beb
Jaak Ristioja <jaak.ristioja@cyber.ee>
2015-02-16 14:20:18 +0200

    TdbVectorMapUtil: Fixed a TODO, some optimizations.

--- src/TdbVectorMapUtil.cpp
+++ src/TdbVectorMapUtil.cpp
@@ -11,7 +11,7 @@
 #include "TdbVectorMapUtil.h"
 
 #include <sharemind/Random/IRandom.h>
-#include <sstream>
+#include <string>
 #include "TdbVectorMap.h"
 
 
@@ -80,52 +80,47 @@ void destroy(void * ptr) noexcept { delete static_cast<T *>(ptr); }
 namespace sharemind {
 
 TdbVectorMapUtil::TdbVectorMapUtil()
-{
-    m_wrapper.internal = this;
-    m_wrapper.new_map = &SharemindTdbVectorMapUtil_new_map;
-    m_wrapper.delete_map = &SharemindTdbVectorMapUtil_delete_map;
-    m_wrapper.get_map = &SharemindTdbVectorMapUtil_get_map;
-}
+    : m_wrapper{this,
+                &SharemindTdbVectorMapUtil_new_map,
+                &SharemindTdbVectorMapUtil_delete_map,
+                &SharemindTdbVectorMapUtil_get_map}
+{}
 
-TdbVectorMap * TdbVectorMapUtil::newVectorMap(SharemindDataStore * dataStore) const {
+TdbVectorMap * TdbVectorMapUtil::newVectorMap(SharemindDataStore * dataStore)
+        const
+{
     assert(dataStore);
 
     uint64_t vmapId = 0;
-    std::ostringstream oss; // TODO for C++11 replace with std::to_string()
+    std::string s;
 
-    // Generate an identifier
+    // Generate an unique identifier:
     do {
-        oss.str(""); oss.clear(); oss << ++vmapId;
-    } while (!!dataStore->get(dataStore, oss.str().c_str()));
+        s = std::to_string(++vmapId);
+    } while (!!dataStore->get(dataStore, s.c_str()));
 
-    // Store the map
-    TdbVectorMap * map = new TdbVectorMap(vmapId);
-    if (!dataStore->set(dataStore, oss.str().c_str(), map, &destroy<TdbVectorMap>)) {
-        delete map;
-        return nullptr;
-    }
+    // Store the map:
+    TdbVectorMap * const map = new TdbVectorMap{vmapId};
+    if (dataStore->set(dataStore, s.c_str(), map, &destroy<TdbVectorMap>))
+        return map;
 
-    return map;
+    delete map;
+    return nullptr;
 }
 
-bool TdbVectorMapUtil::deleteVectorMap(SharemindDataStore * dataStore, const uint64_t vmapId) const {
+bool TdbVectorMapUtil::deleteVectorMap(SharemindDataStore * dataStore,
+                                       const uint64_t vmapId) const
+{
     assert(dataStore);
-
-    std::ostringstream oss;
-    oss << vmapId;
-
-    // Store the map
-    return dataStore->remove(dataStore, oss.str().c_str());
+    return dataStore->remove(dataStore, std::to_string(vmapId).c_str());
 }
 
-TdbVectorMap * TdbVectorMapUtil::getVectorMap(SharemindDataStore * dataStore, const uint64_t vmapId) const {
+TdbVectorMap * TdbVectorMapUtil::getVectorMap(SharemindDataStore * dataStore,
+                                              const uint64_t vmapId) const
+{
     assert(dataStore);
-
-    std::ostringstream oss;
-    oss << vmapId;
-
-    // Get the map from the store
-    return static_cast<TdbVectorMap *>(dataStore->get(dataStore, oss.str().c_str()));
+    return static_cast<TdbVectorMap *>(
+                dataStore->get(dataStore, std::to_string(vmapId).c_str()));
 }
 
 } /* namespace sharemind { */
